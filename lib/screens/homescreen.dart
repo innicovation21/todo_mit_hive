0d110befe05afd546fb0a37bc411e4ff986f3c30
innicovation21 <95610543+innicovation21@users.noreply.github.com>
2023-03-26 21:44:07 +0200

    Update homescreen.dart

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import '../data/database.dart';
-import '../util/dialog_box.dart';
-import '../util/todo_tile.dart';
+import 'package:todomithive/widgets/buttons.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+import '../database/database.dart';
+import '../widgets/alertdialogs.dart';
+import '../widgets/tiles.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeScreenState extends State<HomeScreen> {
   // Referenzierung der Hive Box
   final toDoBox = Hive.box('toDoBox');
   ToDoDataBase db = ToDoDataBase();
 
   @override
   void initState() {
-    // wenn es noch keine Datenbank mit dem Namen 'TODOLIST' gibt... 
+    // wenn es noch keine Datenbank mit dem Namen 'TODOLIST' gibt...
     if (toDoBox.get("TODOLIST") == null) {
       //...dann erstelle so eine Datenbank...
       db.createInitialData();
@@ -36,7 +38,7 @@ class _HomePageState extends State<HomePage> {
   // Funktion für check / uncheck des checkbox widgets
   void checkBoxChanged(bool? value, int index) {
     setState(() {
-      db.toDoList[index][1] = !db.toDoList[index][1];
+      db.toDoListe[index][1] = !db.toDoListe[index][1];
     });
     db.updateDataBase();
   }
@@ -44,7 +46,7 @@ class _HomePageState extends State<HomePage> {
   // funktion zum updaten/speichern einer neuen Aufgabe
   void saveNewTask() {
     setState(() {
-      db.toDoList.add([_controller.text, false]);
+      db.toDoListe.add([_controller.text, false]);
       _controller.clear();
     });
     Navigator.of(context).pop();
@@ -56,7 +58,7 @@ class _HomePageState extends State<HomePage> {
     showDialog(
       context: context,
       builder: (context) {
-        return DialogBox(
+        return AddTaskDialog(
           controller: _controller,
           onSave: saveNewTask,
           onCancel: () => Navigator.of(context).pop(),
@@ -68,7 +70,7 @@ class _HomePageState extends State<HomePage> {
   // Funktion zum Löschen einer Aufgabe
   void deleteTask(int index) {
     setState(() {
-      db.toDoList.removeAt(index);
+      db.toDoListe.removeAt(index);
     });
     db.updateDataBase();
   }
@@ -87,11 +89,11 @@ class _HomePageState extends State<HomePage> {
         child: Icon(Icons.add),
       ),
       body: ListView.builder(
-        itemCount: db.toDoList.length,
+        itemCount: db.toDoListe.length,
         itemBuilder: (context, index) {
           return ToDoTile(
-            taskName: db.toDoList[index][0],
-            taskCompleted: db.toDoList[index][1],
+            taskName: db.toDoListe[index][0],
+            taskCompleted: db.toDoListe[index][1],
             onChanged: (value) => checkBoxChanged(value, index),
             deleteFunction: (context) => deleteTask(index),
           );
